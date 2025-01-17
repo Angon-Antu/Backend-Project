f5298a1010c1b78f1adeee6e297b8533a9410c4d
Angon-Antu <129616238+Angon-Antu@users.noreply.github.com>
2025-01-17 20:19:27 +0600

    Add files via upload

---
+++ routes/helloRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { getHello } = require('../controllers/helloController');
+const router = express.Router();
+
+router.get('/', getHello);
+
+module.exports = router;
