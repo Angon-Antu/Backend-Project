9a147769492c030794b455299fe8cae2287d7aa2
Angon-Antu <129616238+Angon-Antu@users.noreply.github.com>
2025-01-17 20:14:29 +0600

    Add files via upload

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,12 @@
+const jwt = require('jsonwebtoken');
 
+const createToken = (req, res) => {
+  const token = jwt.sign({ user: 'testUser' }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  res.json({ token });
+};
+
+const postResponse = (req, res) => {
+  res.send('I am post body');
+};
+
+module.exports = { createToken, postResponse };
---
+++ controllers/helloController.js
@@ -0,0 +1,6 @@
+const getHello = (req, res) => {
+    res.send('Hello World');
+  };
+  
+  module.exports = { getHello };
+  
